131948c804840635c758e33c92f43f9a043f9dda
codingisveryhard <lunaticdawn1203@gmail.com>
2025-01-08 13:09:53 +0900

    주석 추가 및 약간의 수정
    주석을 추가하였고 move 메서드에 너무 많은 코드가 몰려있다고 느껴서 BeginPlay로 반복되는 부분을 옮겨서 분리시켰습니다.

--- Source/hello/MyActor.cpp
+++ Source/hello/MyActor.cpp
@@ -12,7 +12,7 @@ AMyActor::AMyActor()
 
 
 int AMyActor::step() {
-	return FMath::RandRange(0, 1);
+	return FMath::RandRange(0, 1);	// 0과 1 중 하나를 랜덤으로 반환
 }
 
 float AMyActor::distance(FVector2D first, FVector2D second)
@@ -20,37 +20,34 @@ float AMyActor::distance(FVector2D first, FVector2D second)
 	float x = first.X - second.X;
 	float y = second.Y - first.Y;
 
-	return FMath::Sqrt(FMath::Pow(x, 2) + FMath::Pow(y, 2));
+	return FMath::Sqrt(FMath::Pow(x, 2) + FMath::Pow(y, 2)); // 이동거리를 주어진 식에 따라 계산
 }
 
 int AMyActor::createEvent() {
-	UE_LOG(LogTemp, Log, TEXT("이벤트가 발생되었습니다."));
-	return evCnt++;
+	UE_LOG(LogTemp, Log, TEXT("이벤트가 발생되었습니다."));	// 이벤트 발생 시 출력
+	return evCnt++;											// 이벤트 카운트 1 증가
 }
 
 
 void AMyActor::move() {
-	for (int i = 1; i <= 10; i++) {
-	int x = step();
-	int y = step();
-	FVector2D init = start;
-	if (FMath::RandRange(0, 1))	createEvent();
-
-	start.X += x;
-	start.Y += y;
-	totDist += distance(init, start);
-	
-		UE_LOG(LogTemp, Warning, TEXT("%d번째 행동: 현재 위치는: (%d, %d) 이동한 거리는 : %f"), i, (int)start.X, (int)start.Y, distance(init, start));
-	}
-	UE_LOG(LogTemp, Log, TEXT("총 %d회의 이벤트와 %f의 거리만큼 이동했습니다."), evCnt, totDist);
+		int x = step();								// x축으로 이동시킬 값을 반환
+		int y = step();								// y축으로 이동시킬 값을 반환
+		start.X += x;								// 현재 위치에 x축에 반환된 값만큼 증가
+		start.Y += y;								// 현재 위치의 y축에 반환된 값만큼 증가
 }
 
 // Called when the game starts or when spawned
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	move();
-
+	for (int i = 1; i <= 10; i++) {					// 이동을 10회 반복
+		FVector2D init = start;						// 초기의 위치값을 저장
+		move();										// 이동 함수
+		if (FMath::RandRange(0, 1))	createEvent();	// 50%의 확률로 작동하는 이벤트 함수
+		totDist += distance(init, start);			// 총 이동량에 이동거리만큼 추가
+		UE_LOG(LogTemp, Warning, TEXT("%d번째 행동: 현재 위치는: (%d, %d) 이동한 거리는 : %f"), i, (int)start.X, (int)start.Y, distance(init, start)); // 현재 위치와 이동거리를 출력
+	}
+	UE_LOG(LogTemp, Log, TEXT("총 %d회의 이벤트와 %f의 거리만큼 이동했습니다."), evCnt, totDist);	// 전체 이동을 마친 후 출력
 }
 
 // Called every frame
--- Source/hello/MyActor.h
+++ Source/hello/MyActor.h
@@ -12,18 +12,18 @@ class HELLO_API AMyActor : public AActor
 	GENERATED_BODY()
 
 private:
-	FVector2D start = { 0, 0 };
-	int evCnt = 0;
-	float totDist;
+	FVector2D start = { 0, 0 };		// 시작 지점 설정
+	int evCnt = 0;					// 이벤트 카운트 수 변수 선언
+	float totDist;					// 총 이동거리 변수 선언
 	
 public:	
 	// Sets default values for this actor's properties
 	AMyActor();
 
-	void move();
-	int step();
-	float distance(FVector2D first, FVector2D second);
-	int createEvent();
+	void move();					// 캐릭터의 움직임을 구현한 함수
+	int step();						// 캐릭터가 한 번 이동하는 정도를 구현하는 함수
+	float distance(FVector2D first, FVector2D second);	// 캐릭터가 이동한 거리를 계산하는 함수
+	int createEvent();				// 이벤트가 발생할 때 작동하는 함수
 
 protected:
 	// Called when the game starts or when spawned
